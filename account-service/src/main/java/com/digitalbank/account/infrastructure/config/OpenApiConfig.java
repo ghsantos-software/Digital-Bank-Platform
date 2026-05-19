636f67d4104b18c9a3a383e17bc8dba7480376e8
Guilherme Henrique <guihenrique.s@outlook.com>
2026-05-19 11:47:08 -0300

    update swagger contact url

--- account-service/src/main/java/com/digitalbank/account/infrastructure/config/OpenApiConfig.java
+++ account-service/src/main/java/com/digitalbank/account/infrastructure/config/OpenApiConfig.java
@@ -35,7 +35,7 @@ public class OpenApiConfig {
                         .version("1.0.0")
                         .contact(new Contact()
                                 .name("Digital Bank Platform")
-                                .url("https://github.com/your-username/digital-bank-platform")))
+                                .url("https://github.com/gh1santos/Digital-Bank-Platform")))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
